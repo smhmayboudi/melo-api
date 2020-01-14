474c550d2f92735cad363574f3bfd5763512ae91
Hossein Mayboudi <smhmayboudi@gmail.com>
2020-01-14 22:19:15 +0330

    Fix error interceptro

--- src/Interceptor/error.interceptor.ts
+++ src/Interceptor/error.interceptor.ts
@@ -7,16 +7,10 @@ import {
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
-import * as express from "express";
-import { User } from "../user/type/User";
 
 @Injectable()
 export class ErrorInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context
-      .switchToHttp()
-      .getRequest<express.Request & { user: User }>();
-    Logger.log(`request.path: ${request.path}`, "error.interceptor");
+  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error: Error) => {
         Logger.log(`${error}`, "error.interceptor");
