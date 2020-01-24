485202b4752df7257a0355581e11483559a2349c
Hossein Mayboudi <smhmayboudi@gmail.com>
2020-01-24 03:45:55 +0330

    Refactor error interceptor

--- src/interceptor/error.interceptor.ts
+++ src/interceptor/error.interceptor.ts
@@ -5,15 +5,27 @@ import {
   Logger,
   NestInterceptor
 } from "@nestjs/common";
+import * as express from "express";
 import { Observable } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { JwtPayloadDto } from "src/auth/dto/jwt.payload.dto";
 
 @Injectable()
 export class ErrorInterceptor implements NestInterceptor {
-  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context
+      .switchToHttp()
+      .getRequest<express.Request & { user: JwtPayloadDto }>();
     return next.handle().pipe(
       catchError((error: Error) => {
-        Logger.log(`${error}`, "error.interceptor");
+        Logger.error(error, "error.interceptor");
+        Logger.error(
+          JSON.stringify({
+            requestPath: request.path,
+            requestUser: request.user
+          }),
+          "error.interceptor"
+        );
         throw error;
       })
     );
