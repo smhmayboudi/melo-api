00dd589187a8bd2169b0ec90f11c46ac0684dde6
Hossein Mayboudi <smhmayboudi@gmail.com>
2020-01-25 18:01:11 +0330

    Refactor telegram module

---
+++ src/telegram/dto/telegram.send.song.dto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber } from "class-validator";
+
+export class TelegramSendSongDto {
+  constructor(id: number) {
+    this.id = id;
+  }
+
+  @ApiProperty({
+    description: "The song identification",
+    example: 0
+  })
+  @IsNumber()
+  id: number;
+}
