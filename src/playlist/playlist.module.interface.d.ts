1ddc9df5b04190dae0fe11cf23662e0716525dac
Hossein Mayboudi <smhmayboudi@gmail.com>
2020-03-06 17:55:20 +0330

    test(playlist): Test completed

--- src/playlist/playlist.module.interface.d.ts
+++ src/playlist/playlist.module.interface.d.ts
@@ -1,11 +1,11 @@
 import { Document } from "mongoose";
 
-export interface Playlist extends Document {
+export interface PlaylistInterface extends Document {
   downloads_count: number;
   followers_count: number;
+  isPublic: boolean;
   owner_user_id: string;
   photo_id: string;
-  isPublic: boolean;
   release_date: Date;
   songs_ids: number[];
   title: string;
