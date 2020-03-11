f3873bb42d7a3a36ebde0ea49394ab53512bb758
Bamdad <nouri.bamdad@gmail.com>
2020-03-11 13:59:47 +0330

    test(app): Complete app tests

--- src/app/app.http-exception.filter.spec.ts
+++ src/app/app.http-exception.filter.spec.ts
@@ -1,7 +1,24 @@
 import { AppHttpExceptionFilter } from "./app.http-exception.filter";
 
 describe("HttpExceptionFilter", () => {
+  const host: any = {
+    switchToHttp: jest.fn().mockReturnThis(),
+    getRequest: jest.fn(() => ({ method: "", route: { path: "" } })),
+    getResponse: jest.fn(() => ({ status: () => ({ json: () => ({}) }) }))
+  };
+  const exception: any = {
+    getStatus: jest.fn(() => 200)
+  };
+
   it("should be defined", () => {
     expect(new AppHttpExceptionFilter()).toBeDefined();
+  });
+
+  it("catch should be undefined", () => {
+    expect(new AppHttpExceptionFilter().catch(exception, host)).toBeUndefined();
+    expect(host.switchToHttp).toHaveBeenCalled();
+    expect(host.getRequest).toHaveBeenCalled();
+    expect(host.getResponse).toHaveBeenCalled();
+    expect(exception.getStatus).toHaveBeenCalled();
   });
 });
