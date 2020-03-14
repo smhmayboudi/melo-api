2294f4aad233b3c29875259f3c1f228434f0740f
Bamdad <nouri.bamdad@gmail.com>
2020-03-14 16:57:29 +0330

    test(app): Fix pipe tests

--- src/app/app.hash-id.pipe.spec.ts
+++ src/app/app.hash-id.pipe.spec.ts
@@ -9,7 +9,7 @@ describe("AppHashIdPipe", () => {
     encode: (): string => ""
   };
 
-  let appHashIdPipe: AppHashIdPipe;
+  let service: AppHashIdService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
@@ -21,11 +21,21 @@ describe("AppHashIdPipe", () => {
         }
       ]
     }).compile();
-    appHashIdPipe = module.get<AppHashIdPipe>(AppHashIdPipe);
+    service = module.get<AppHashIdService>(AppHashIdService);
   });
 
   it("should be equal to a value", () => {
-    expect(appHashIdPipe.transform("", { type: "body" })).toEqual(0);
+    expect(new AppHashIdPipe(service).transform("", { type: "body" })).toEqual(
+      0
+    );
+  });
+
+  it("transform should throw bad request error", () => {
+    expect(() =>
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore
+      new AppHashIdPipe(service).transform(0, { type: "body" })
+    ).toThrowError();
   });
 
   it.todo("transform should throw an exception on typeof !== string");
