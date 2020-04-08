ee0071c852da61688a5c72a0a3cdafd294dd7622
Bamdad <nouri.bamdad@gmail.com>
2020-04-08 12:27:29 +0430

    fix(auth): Fix anonym strategy

--- src/auth/anonym-uuid.strategy.ts
+++ src/auth/anonym-uuid.strategy.ts
@@ -1,13 +1,17 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AuthStrategyResDto } from "./dto/res/auth.strategy.res.to";
-import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-anonym-uuid";
+// import { Strategy } from "passport-anonym-uuid";
+import { Strategy } from "./anonym.strategy";
 
 @Injectable()
 export class AnonymUUIDStrategy extends PassportStrategy(Strategy) {
-  async validate(): Promise<AuthStrategyResDto> {
-    return Promise.resolve({
-      sub: "0"
-    });
+  async validate(au: any): Promise<AuthStrategyResDto> {
+    if (au === undefined) {
+      return Promise.resolve({
+        sub: "0",
+      });
+    }
+    throw new UnauthorizedException();
   }
 }
